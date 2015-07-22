1a103496726c9b3c4010bb0a01d62364685aa004
Bazyl Anton <php.drvn@gmail.com>
2015-07-22 13:38:47 +0300

    no message

--- admin/language/english/module/d_seo.php
+++ admin/language/english/module/d_seo.php
@@ -5,7 +5,7 @@
 
 // Heading
 $_['heading_title']									= '<span style="color:#449DD0; font-weight:bold">Standard Module</span><span style="font-size:12px; color:#999"> by <a href="http://www.opencart.com/index.php?route=extension/extension&filter_username=Dreamvention" style="font-size:1em; color:#999" target="_blank">Dreamvention</a></span>';
-$_['heading_title_main']				= 'Standard Module';
+$_['heading_title_main']				= 'Seo Module';
 $_['text_edit']													= 'Edit Standard Module settings';
 
 
