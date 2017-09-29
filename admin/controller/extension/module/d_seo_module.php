06857a2538973f37dddce4cd27f3f91eccce833a
belikov_artem <dev.dreamvention@gmail.com>
2017-09-29 14:50:28 +0300

    правки

--- admin/controller/extension/module/d_seo_module.php
+++ admin/controller/extension/module/d_seo_module.php
@@ -1396,13 +1396,7 @@ class ControllerExtensionModuleDSEOModule extends Controller {
 				
 		if ($this->validateInstall()) {
 			$this->{'model_extension_module_' . $this->codename}->installModule();
-			
-			if (file_exists(DIR_APPLICATION . 'model/extension/module/d_twig_manager.php')) {
-				$this->load->model('extension/module/d_twig_manager');
-			
-				$this->model_extension_module_d_twig_manager->installCompatibility();
-			}
-			
+						
 			if (file_exists(DIR_APPLICATION . 'model/extension/module/d_event_manager.php')) {
 				$this->load->model('extension/module/d_event_manager');
 				
@@ -1570,6 +1564,12 @@ class ControllerExtensionModuleDSEOModule extends Controller {
 			$this->load->model('extension/d_shopunity/mbooth');
 			
 			$this->model_extension_d_shopunity_mbooth->installDependencies($this->codename);
+		}
+		
+		if (file_exists(DIR_APPLICATION . 'model/extension/module/d_twig_manager.php')) {
+			$this->load->model('extension/module/d_twig_manager');
+			
+			$this->model_extension_module_d_twig_manager->installCompatibility();
 		}
 	}
 		
